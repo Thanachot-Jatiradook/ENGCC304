2660db2b5e7ad2b836715839efe08b8f411fe9fb
Thanachot Jatiradook <jatiradook13@gmail.com>
2025-07-16 20:38:03 +0700

    will12

---
+++ hw01/hw01-3-basic-input-output.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ int main(){
+
+    char First_Name[50];
+    char Last_Name[50];
+
+    printf("First Name: ");
+    scanf("%s", First_Name);
+    printf("Last Name:");
+    scanf("%s", Last_Name);
+
+    printf("%s ", First_Name);
+    printf("%s ", Last_Name);
+    printf("TC, RMUTL, Chiang Mai, Thailand");
+ }
