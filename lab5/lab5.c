2a12c6bb1da233b9aee4b5a61e142f42e5f28141
Thanachot Jatiradook <jatiradook13@gmail.com>
2025-07-23 12:08:04 +0700

    will14

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -2,9 +2,39 @@
 
 int main(){
        int score = 0 ;
+       char check;
         
        printf("enter score :\n");
-       scanf("%d", &score);
 
-       printf("%d", score);
- }
+       if (scanf("%d", &score) != 1) {
+        printf("please enter number only.\n");
+       }
+
+       else {
+
+              if (score >= 80){
+                     printf("A !");
+              } 
+              else if (score >= 75 && score < 80){
+                     printf("B+ !");
+              }
+              else if (score >= 70 && score < 75){
+                     printf("B !");
+              }
+              else if (score >= 65 && score < 70){
+                     printf("C+ !");
+              }
+              else if (score >= 60 && score < 65){
+                     printf("C !");
+              }
+              else if (score >= 55 && score < 60){
+                     printf("D+ !");
+              }
+              else if (score >= 50 && score < 55){
+                     printf("D !");
+              }
+              else if (score < 50){
+                     printf("F !");
+              }
+       }
+}
