9d7466dc997e07b56fa4e130b68de68631e54355
Thanachot Jatiradook <jatiradook13@gmail.com>
2025-08-01 17:32:21 +0700

    will19

--- lab6/lab6.cpp
+++ lab6/lab6.cpp
@@ -1,27 +1,30 @@
 #include <stdio.h>
-int main(){
 
-    int n = 0 ;
-    int i = 0 ;
-    
-    printf( "Enter value:\n" ) ;
-    scanf( "%d" , &n ) ;
+int main() {
+    int n;
 
-    printf( "Series: " ) ;
-    
-    if (n % 2 == 1 ){
-        for (int i = 1; i <=n; i+=2){
-            printf( "%d " , i ) ;
+    printf("Enter value:\n");
+    if (scanf("%d", &n) != 1) {
+        printf("Please Enter Number Only\n");
+        return 1;
+        
+    }//end if 
+
+    printf("Series: ");
+
+    if (n % 2 == 1) {
+        
+        for (int i = 1; i <= n; i += 2) {
+            printf("%d ", i);
         }
-    }//end if
-    else {
-        for (int i = n; i >=0; i -= 2){
-            printf( "%d " , i ) ;
+    }//end if  
+        else {
+        
+        for (int i = n; i >= 0; i -= 2) {
+            printf("%d ", i);
         }
     }//end else
 
-
-    
-
-
-}//end function
+    printf("\n");
+    return 0;
+}
